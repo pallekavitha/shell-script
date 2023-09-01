020d85ba4349d8e48d713d3a0f8c0e0ee68cbf06
pallekavitha <kavithapallewar007@gmail.com>
2023-09-02 00:34:27 +0530

    array script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,10 +4,11 @@ echo "please enter your username"
 
 read -s USERNAME # this is the variable where the value of the one user entered
 
+#this is only for validation, we should not print in the terminal
 echo "Username entered is: $USERNAME"
 
 echo "please enter your password"
 
-read -s PASSWORD
+read -s  PASSWORD
 
 echo "Password entered is: $PASSWORD"
