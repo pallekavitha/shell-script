e5be429fb2b746aeab1bbdf6d4bf40d14d1f64a5
pallekavitha <kavithapallewar007@gmail.com>
2023-09-01 23:53:49 +0530

    variables script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,4 +4,4 @@ echo "please enter your username"
 
 echo USERNAME # this is the variable where the value of the one user entered
 
-echo "Username entered is: $USERNAME"
+echo "Username entered is:$USERNAME"
