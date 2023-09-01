155c212000f11b66322979fbdc69c3b78a95172f
pallekavitha <kavithapallewar007@gmail.com>
2023-09-01 23:17:49 +0530

    variables script added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello-world I am learning DevOps with AWS"
+PERSON1=$1 # this is the first value from the command line
+PERSON2=$2 # this is the second value from the command line
+
+echo "$PERSON1 : Hi $PERSON2, good morning"
+echo "$PERSON2 : Hi, good morning $PERSON1"
+echo "$PERSON1 : How are you $PERSON2"
+echo "$PERSON2 : I am good, how are you $PERSON1"
