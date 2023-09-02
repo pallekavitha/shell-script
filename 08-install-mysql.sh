2da0638e38b5ded7301f8010247fc43691e7e770
pallekavitha <kavithapallewar007@gmail.com>
2023-09-02 11:00:05 +0530

    mysql script added

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -11,5 +11,14 @@ then
 else
     echo "INFO:: you are root user"
 fi
+ 
+ # it is our responsibility again to check installation is success or not
+yum install mysqlll -y
 
-yum install mysql -y
+if [ $? -ne 0 ]
+then 
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
